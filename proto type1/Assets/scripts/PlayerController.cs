8efdc096129e1924aca019f9ccbcc098feeaa78d
natycg <checon@roverkids.org>
2024-11-19 14:07:22 -0500

    code 2 translate

--- proto type1/Assets/scripts/PlayerController.cs
+++ proto type1/Assets/scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move the vechile forward
+        transform.Translate(0, 0, 1);
     }
 }
